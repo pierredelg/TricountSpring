2b6e1ea9f5d81a357a925aaab8b0f8a7a84ec120
Pierre Delgrange <pierredelgrange@hotmail.fr>
2020-01-13 23:37:58 +0100

    debut du projet

---
+++ src/main/java/com/da2i/TricountDa2i/TricountDa2iApplication.java
@@ -0,0 +1,13 @@
+package com.da2i.TricountDa2i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TricountDa2iApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TricountDa2iApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/da2i/TricountDa2i/TricountDa2iApplicationTests.java
@@ -0,0 +1,13 @@
+package com.da2i.TricountDa2i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TricountDa2iApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
