c17cd2fa6a9ea65fb282d4d09117cbfcdd0b9481
Pierre Delgrange <pierredelgrange@hotmail.fr>
2020-01-20 20:11:37 +0100

    ajout service

--- src/main/java/com/da2i/tricountda2i/config/SwaggerConfig.java
+++ src/main/java/com/da2i/tricountda2i/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.da2i.tricountda2i.service"))
+                .apis(RequestHandlerSelectors.basePackage("com.da2i.tricountda2i.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
