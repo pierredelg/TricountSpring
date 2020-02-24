33264fff00ecf9c2703d8848e7ae4145afdd01ea
Pierre Delgrange <pierredelgrange@hotmail.fr>
2020-02-24 22:42:03 +0100

    ajout du mail de bienvenue

--- src/main/java/com/da2i/tricountda2i/service/EmailService.java
+++ src/main/java/com/da2i/tricountda2i/service/EmailService.java
@@ -3,6 +3,8 @@ package com.da2i.tricountda2i.service;
 import com.da2i.tricountda2i.model.Utilisateur;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -16,32 +18,39 @@ import java.util.Map;
 
 @Service
 public class EmailService {
+
+    Logger logger = LoggerFactory.getLogger(EmailService.class);
+
     @Autowired
     private JavaMailSender sender;
 
     @Autowired
     private Configuration freemarkerConfig;
 
-    public void sendWelcomeEmail(Utilisateur utilisateur) throws Exception {
+    public void sendWelcomeEmail(Utilisateur utilisateur){
 
         MimeMessage message = sender.createMimeMessage();
+        try {
 
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
-        freemarkerConfig.setClassForTemplateLoading(this.getClass(), "/templates/");
+            freemarkerConfig.setClassForTemplateLoading(this.getClass(), "/templates/");
 
-        Template template = freemarkerConfig.getTemplate("welcomeMail.ftlh");
-        Map<String, Object> model = new HashMap<>();
-        model.put("nom", utilisateur.getNom());
+            Template template = freemarkerConfig.getTemplate("welcomeMail.html");
+            Map<String, Object> model = new HashMap<>();
+            model.put("nom", utilisateur.getNom());
 
-        String text = FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
+            String text = FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
 
-        helper.setTo(utilisateur.getEmail());
-        helper.setText(text,true);
-        helper.addInline("header", new ClassPathResource("headerLogo.png"));
-        helper.addInline("iphone", new ClassPathResource("iphone-fr.png"));
-        helper.setSubject("Bienvenue sur Tricount DA2I");
-
+            helper.setTo(utilisateur.getEmail());
+            helper.setText(text, true);
+            helper.addInline("header", new ClassPathResource("headerLogo.png"));
+            helper.addInline("iphone", new ClassPathResource("iphone-fr.png"));
+            helper.setSubject("Bienvenue sur Tricount DA2I");
+        }catch (Exception e){
+            logger.error("ENVOI EMAIL IMPOSSIBLE : ADRESSE= "+ utilisateur.getEmail());
+        }
         sender.send(message);
+        logger.info("ENVOI EMAIL: ADRESSE= "+ utilisateur.getEmail());
     }
 }
